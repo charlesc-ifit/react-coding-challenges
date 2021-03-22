d07fe623ebb75ec249a24902973b9926e83c5def
Charles Cook <charlesc@ifit.com>
2021-03-22 09:11:38 -0400

    refactor: Discover Container

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
 
-export default class Discover extends Component {
+function Discover({ newReleases, playlists, categories }) {
+  return (
+    <div className="discover">
+      <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
+      <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
+      <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+    </div>
+  );
+}
+
+export default class DiscoverContainer extends Component {
   constructor() {
     super();
 
@@ -17,11 +27,11 @@ export default class Discover extends Component {
     const { newReleases, playlists, categories } = this.state;
 
     return (
-      <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
-      </div>
+      <Discover
+        newReleases={newReleases}
+        playlists={playlists}
+        categories={categories}
+      />
     );
   }
 }
